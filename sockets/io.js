57290a768be3d1122b2b8d8384f2911e6f084b85
Jacob Hildebrandt <jake@hildy.ca>
2021-05-28 09:48:37 -0700

    Removed unnecessary socket folder and io.js file

--- sockets/io.js
+++
@@ -1,9 +0,0 @@
-const io = require("./../server");
-
-io.on("connection", (socket) => {
-  console.log("id", socket.id);
-  socket.emit("messageFromServer", { data: "Welcome to the socketio server" });
-  socket.on("messageToServer", (dataFromClient) => {
-    console.log(dataFromClient);
-  });
-});
